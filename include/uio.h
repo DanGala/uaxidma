bbe8f5c67c455e3e4e94a6b44cd376ea48c4446a
DanGala <daniel.gmontes.94@gmail.com>
2023-12-03 12:03:03 +0100

    [doc] Move doxygen to UIO header file

--- include/uio.h
+++ include/uio.h
@@ -7,17 +7,42 @@
 class uio_device
 {
 public:
+    /**
+     * @brief Construct a new UIO device
+     * @param name the UIO device property in the /sys/class/uio/uioN/name file should match this name
+     * @note Aborts execution if the device is not found
+     */
     uio_device(const std::string &name);
+
+    /**
+     * @brief Destroy the uio device releasing associated system resources
+     */
     ~uio_device();
+
+    /**
+     * @brief Creates a virtual mapping of the UIO device's memory space
+     * @return virtual address where the device is mapped, nullptr if map failed
+     */
     uint8_t *map();
+
+    /**
+     * @brief Deallocates the virtual mapping of the UIO device's memory
+     * @return false on errors 
+     */
     bool unmap();
 
-    int fd;
+    int fd; //!< File descriptor number associated to the device
+
 private:
+    /**
+     * @brief Finds and sets the uio device number by device name
+     * @param name Name of the UIO device as in the /sys/class/uio/uioN/name file
+     * @return false on errors
+     */
     bool find_by_name(const std::string &name);
 
-    int number_;
-    uint8_t *virt_addr_;
+    int number_; //!< UIO device number as found in /dev/uio<N> and /sys/class/uio/uio<N>
+    uint8_t *virt_addr_; //!< Virtual mapping address
 };
 
 #endif /* _UIO_H */
